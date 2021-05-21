9a7a8359b0a58e6af0cc3fcf18eb982c0bf7c483
encodedguy <65826142+encodedguy@users.noreply.github.com>
2021-05-21 19:09:35 +0530

    Added c file and executable

---
+++ crash.c
@@ -0,0 +1,19 @@
+//SCRIPT WRITTEN HERE
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	long int data[50];
+	struct node* next;
+};
+
+int main(){
+	while(1==1){
+		struct node* head = (struct node*)malloc(sizeof(struct node));
+	}
+}
+
+
+
+//SCRIPT WRITTEN HERE
